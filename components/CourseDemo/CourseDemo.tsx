abc15a0572f5fe8fb50092c130220eb9fc73514c
Emil Szymczyk <emil-szymczyk2@wp.pl>
2025-01-05 22:01:53 +0100

    fix demo section responsive styles

--- components/CourseDemo/CourseDemo.tsx
+++ components/CourseDemo/CourseDemo.tsx
@@ -4,30 +4,46 @@ import { Typography } from '../Typography/Typohraphy';
 
 export const CourseDemo = () => {
   return (
-    <Container id="courseDemo" size="lg" my="100px">
-      <Stack gap="xl" align="center">
-        {/* Nagłówek */}
-        <Stack gap="xs" align="center">
-          <Title ta="center" order={2} mb="sm" style={{ color: 'white' }}>
-            Demo kursu
-          </Title>
-          <Typography ta="center" c="dimmed">
-            🎥 Przykładowa lekcja: Zobacz, jak wygląda nauka z nami! <br />
-            Obejrzyj wybrany fragment kursu, by samodzielnie ocenić jakość
-            materiałów.
-          </Typography>
-        </Stack>
+    <Box
+      style={{
+        position: 'relative',
+        overflow: 'hidden',
+        backgroundColor: 'black',
+        padding: '3rem 0',
+      }}
+    >
+      {/* Background Paint Strokes */}
+      {/* <div
+        className={`${styles.backgroundPaint} ${styles.backgroundPaint1}`}
+      ></div>
+      <div
+        className={`${styles.backgroundPaint} ${styles.backgroundPaint2}`}
+      ></div> */}
 
-        {/* Video Player */}
-        <Box style={{ maxWidth: '800px', width: '100%' }}>
-          <VideoPlayer
-            width="800px"
-            videoSrc={
-              'https://drive.google.com/file/d/16YRYhURFzjwIOBOOO9m7oP75JlipDoG8/preview'
-            }
-          />
-        </Box>
-      </Stack>
-    </Container>
+      <Container size="lg">
+        <Stack gap="xl" align="center">
+          <Stack gap={1} align="center">
+            <Title ta="center" order={2} mb="sm" style={{ color: 'white' }}>
+              Demo kursu
+            </Title>
+            <Typography ta="center" c="dimmed">
+              🎥 Przykładowa lekcja: Zobacz, jak wygląda nauka z nami! <br />
+              Obejrzyj wybrany fragment kursu, by samodzielnie ocenić jakość
+              materiałów.
+            </Typography>
+          </Stack>
+
+          {/* Video Section */}
+          <Box style={{ maxWidth: '800px', width: '100%' }}>
+            <VideoPlayer
+              width="800px"
+              videoSrc={
+                'https://drive.google.com/file/d/16YRYhURFzjwIOBOOO9m7oP75JlipDoG8/preview'
+              }
+            />
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
   );
 };
