1d109c9bc16890c4fce39583a1a8fedb6c66b773
Emil Szymczyk <emil-szymczyk2@wp.pl>
2025-01-09 19:59:18 +0100

    api check ip

--- integrations/p24/requestPaymentUrl.ts
+++ integrations/p24/requestPaymentUrl.ts
@@ -4,6 +4,7 @@ import { Order, OrderInput } from './types';
 import { nanoid } from 'nanoid';
 import { Endpoints } from '@/constants/endpoints';
 import prisma from '@/prisma/client';
+import { P24 } from '@ingameltd/node-przelewy24';
 
 export const requestPaymentUrl = async ({
   id,
@@ -11,6 +12,20 @@ export const requestPaymentUrl = async ({
   coursesOffer,
 }: OrderInput) => {
   const sessionId = nanoid();
+
+  const p24 = new P24(
+    +process.env.PAYMENT_MERCHANT_ID!,
+    +process.env.PAYMENT_POS_ID!,
+    "05e8526b7ba646c77fe28fede51a0c4f",
+    "3bd6f66a1948a008",
+
+    {
+      sandbox: false, // enable or disable sandbox
+    }
+  );
+
+  const result = await p24.testAccess();
+  console.log(result);
 
   const payment = await prisma.payment.findFirst({ where: { id } });
 
@@ -44,9 +59,9 @@ export const requestPaymentUrl = async ({
     regulationAccept: false,
   };
 
-  const paymentClient = PaymentClient.getInstance();
+    const paymentClient = PaymentClient.getInstance();
 
-  const { link } = await paymentClient.createTransaction(order);
-
-  return { paymentUrl: link, sessionId };
+    const { link } = await paymentClient.createTransaction(order);
+    return { paymentUrl: link, sessionId };
+  
 };
